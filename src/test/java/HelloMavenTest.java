a1e5078d93f7de5f1a2d864d0bf4240c30aa40b6
unknown <00806935@WASJ46440.amtrak.ad.nrpc>
2015-08-11 09:08:12 -0400

    Initial commit

---
+++ src/test/java/HelloMavenTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+
+public class HelloMavenTest extends FunctionalTestCase {
+	
+	@Test
+	public void sayHelloReturnsHelloMaven() throws Exception{
+		runFlowAndExpect("sayHello", "Hello Maven");
+	}
+	@Override
+	protected String getConfigFile() {
+		// TODO Auto-generated method stub
+		return "maven-project.xml";
+	}
+
+}
